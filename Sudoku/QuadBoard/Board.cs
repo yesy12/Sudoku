2b4821050f2ab98250eef171e9d74d7d427d0074
Alison Vieira <alisonvieira.av29@gmail.com>
2025-12-18 16:24:52 -0300

    Board is created

---
+++ Sudoku/QuadBoard/Board.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Sudoku.Nodes;
+using Sudoku.Groups;
+
+namespace Sudoku.QuadBoard {
+    public class Board {
+
+        private int quantity;
+        public Lines lines;
+        public Columns columns;
+        public Group group;
+
+        public Board(int quantity) {
+            this.quantity = quantity;
+            lines = new Lines(quantity);
+            columns = new Columns(quantity);
+            group = new Group(quantity);
+        }
+
+        public void AddCell(NodeCell cell, int lineIndex, int columnIndex) {
+            lines.Add(cell, lineIndex, columnIndex);
+            columns.Add(cell, columnIndex, lineIndex);
+            group.Add(cell, (lineIndex / 3) * 3 + columnIndex / 3, (lineIndex % 3) * 3 + columnIndex % 3);
+        }
+
+        public void LineToString() => lines.ToString();
+        public void ColumnToString() => columns.ToString();
+        public void GroupToString() => group.ToString();
+
+    }
+}
