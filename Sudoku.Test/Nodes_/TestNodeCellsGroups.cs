134ee33f08a2011299e0d7713ee67a1f8b18798b
Alison Vieira <alisonvieira.av29@gmail.com>
2025-12-18 04:17:19 -0300

    Test Node Cells Groups

---
+++ Sudoku.Test/Nodes_/TestNodeCellsGroups.cs
@@ -0,0 +1,23 @@
+﻿using Sudoku.Nodes;
+namespace Sudoku.Test;
+
+public class TestNodeCellsGroups{
+    private NodeCellsGroup Group;
+    private int quantity = 9;
+    [SetUp]
+    public void Setup(){
+        Group = new NodeCellsGroup(quantity);
+    }
+
+    [Test]
+    public void TestAddCellsToGroup(){
+        for (byte i = 1; i < quantity + 1; i++) 
+            Group.Add(i, i - 1);
+
+        for (int i = 0; i < quantity; i++)
+            Assert.That((byte)(i + 1), Is.EqualTo(Group.Cells[i].Number));
+        
+    }
+
+
+}
