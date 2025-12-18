c807f8f02077c9d108af7ef6bd7c86b694943c88
Alison Vieira <alisonvieira.av29@gmail.com>
2025-12-18 16:25:32 -0300

    Board is call in program.cs

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,26 +1,31 @@
-﻿using Sudoku.Nodes;
+﻿using Sudoku.Groups;
+using Sudoku.QuadBoard;
+using Sudoku.Nodes;
 using System;
 
 namespace Sudoku{
     internal static class Program {
 
         public static int quantity = 9;
-        public static NodeCellsGroup Group = new NodeCellsGroup(quantity);
-
+        public static Board board = new Board(quantity);
         private static void Main() {
-            RandomNumberCell.Initialize(quantity);
 
-            for (byte i = 1; i < quantity + 1; i++) 
-                 Group.Add(RandomNumberCell.RandomNumber(), i - 1);
+            for (int i = 0; i < quantity; i++) {
+                for (int j = 0; j < quantity; j++) {
+                    var cell = new NodeCell();
+                    byte value = (byte)((i * 3 + i / 3 + j) % (quantity) + 1);
+                    
+                    cell.Number = value;
+                    board.AddCell(cell,  i, j);
 
-            foreach (var cell in Group.Cells)
-                Console.Write($"{cell.Number} | " );
-            
-            Console.WriteLine("\nPossible Numbers:");
-            RandomNumberCell.Initialize(quantity);
+                }
+            }
 
-            foreach(byte n in RandomNumberCell.possibleNumbers)
-                Console.Write($" {n}");
+            board.LineToString();
+            Console.WriteLine();
+            board.ColumnToString();
+            Console.WriteLine();
+            board.GroupToString();
 
         }
 
